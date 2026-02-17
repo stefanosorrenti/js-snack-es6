d055fc8ba2ba7f63f1043531e3bd0ab426d24e91
stefanosorrenti <stefanosorrenti01@gmail.com>
2026-02-17 14:14:22 +0100

    SNACK 2: add human code.

---
+++ SNACK 2/main.js
@@ -0,0 +1,25 @@
+//TRACCIA
+
+/*
+Snack2
+Creare un array di oggetti di squadre di calcio.
+Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
+Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+*/
+
+//HUMAN CODE
+
+/*
+Cosa devo fare?
+-Creare un array
+-Aggiungere degli oggetti (nel nostro caso 5 squadre di calcio)
+-Ogni oggetto deve avere 3 proprieta: nomi, punti fatti, falli subiti (punti e falli devono essere uguali a 0)
+-Le il valore di punti fatti e falli subiti deve essere un numero generato casualmente
+-Creo una variabile d'appoggio.
+-Ciclo nell'array
+-Seleziono solo le proprieta nomi e falli subiti
+-Loggo tutto in console.
+*/
